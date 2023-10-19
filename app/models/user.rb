b1543b161e6c9149bd4c87332aea1b43e0767d3e
tenntenn6 <jbbcce@gmail.com>
2023-10-19 23:53:17 +0900

    バリデーションの設定・新規登録・ログインのパス設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+
+  validates :nickname,      presence: true
+  validates :birth,         presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
+    validates :family_name_sei
+    validates :family_name_mei
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角(カタカナ)文字を使用してください' } do
+    validates :family_name_kana_sei
+    validates :family_name__kana_mei
+  end
 end
