f9241dc795b9c7d1a220d76540861efe5d2ea34b
tenntenn6 <jbbcce@gmail.com>
2023-10-22 15:18:24 +0900

    Activehash系のコード記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,15 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new 
+  end
+
   def create
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :category_id, :status_id, :prefecture_id, :burden_id, :shipping_date_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,18 @@ class Item < ApplicationRecord
   validates :burden_id        ,presence: true
   validates :shipping_date_id ,presence: true
   validates :price            ,presence: true
-
+  
+  validates :category_id, :status_id, :prefecture_id, :burden_id, :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
 
   belongs_to :user
   has_one_attached :image
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
 end
